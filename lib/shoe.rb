49757e997728576221f642baefd32f2c5c068edf
David zhang <d.ydzdavid@yahoo.com>
2019-04-02 20:57:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+
+class Shoe 
+  attr_accessor :color, :size, :material
+  
+  
+  
+  
+  def initialize (brand)
+    @brand = brand
+  end
+  
+  def condition 
+  end
+
+
+
+
+end
